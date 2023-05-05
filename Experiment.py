276d765c76c4215b6b817a38a77547623f49dff5
esxtang <esxtang@gmail.com>
2023-05-04 23:18:35 -0700

    Make experiment a function so it can be called from notebook

--- Experiment.py
+++ Experiment.py
@@ -1,14 +1,6 @@
-### Start tracking time and memory usage
-# Import time & memory modules
+### Import libraries
 import time
 import tracemalloc
- 
-# Starting the monitoring
-start = time.time()
-tracemalloc.start()
-
-
-### Import libraries
 import pandas as pd
 import numpy as np
 from sklearn.impute import SimpleImputer
@@ -20,39 +12,50 @@ from sklearn.preprocessing import StandardScaler
 from IPython.display import display
 
 
-### Set path for getting data
-PREPROCESSED_DATA_PATH = './PreprocessedData/'
-EXPERIMENT_RESULTS_PATH = './ExperimentResults/'
+### Start tracking time and memory usage 
+# Starting the monitoring
+start = time.time()
+tracemalloc.start()
 
 
-### Get preprocessed data
-labs = pd.read_csv(PREPROCESSED_DATA_PATH + 'preprocessed_labs.csv', index_col = [0, 1], header = [0, 1])
-labs.head()
+### Experiment method
+def experiment(imputation_iterations, hyperparameters, regularization, model_name):
+  # Set path for getting data
+  PREPROCESSED_DATA_PATH = './PreprocessedData/'
+  EXPERIMENT_RESULTS_PATH = './ExperimentResults/'
 
-outcomes = pd.read_csv(PREPROCESSED_DATA_PATH + 'preprocessed_labeled_outcomes.csv', index_col = 0)
-# Translate values to True if Death, False if Alive
-outcomes['Outcome'] = outcomes['Outcome'] == 'Death'
-outcomes.head()
+  # Get preprocessed data
+  labs = pd.read_csv(PREPROCESSED_DATA_PATH + 'preprocessed_labs.csv', index_col = [0, 1], header = [0, 1])
+  outcomes = pd.read_csv(PREPROCESSED_DATA_PATH + 'preprocessed_labeled_outcomes.csv', index_col = 0)
+  outcomes['Outcome'] = outcomes['Outcome'] == 'Death' # Translate values to True if Death, False if Alive
 
+  # Select Patient training set, Labs, Demographic data, and Missingness Indicators
+  train_indices = outcomes.sample(frac = 0.8, random_state = 0).index.sort_values()
+  print('Total patients: {}'.format(len(outcomes)))
+  print('Training patients: {}'.format(len(train_indices)))
 
-### Select Patient training set, Labs, Demographic data, and Missingness Indicators
-train_indices = outcomes.sample(frac = 0.8, random_state = 0).index.sort_values()
+  # Use only latest lab record for each patient
+  latest_labs = labs.groupby('SUBJECT_ID').last()
 
-print('Total patients: {}'.format(len(outcomes)))
-print('Training patients: {}'.format(len(train_indices)))
-
-# Use only latest lab record for each patient
-latest_labs = labs.groupby('SUBJECT_ID').last()
-
-# Flag patients in minority population for Group MICE and Group MICE Missing (TRUE = patient is in minority group)
-minority_population = pd.concat([outcomes['ETHNICITY'] == 'Black', outcomes['GENDER'] == 'Female', outcomes['INSURANCE'] == 'Public'], axis = 1)
-
-# Count columns for Group MICE Missing
-counts = latest_labs.isna().add_suffix('_count')
+  # Impute data, train model, and store predictions for each imputation strategy
+  for strategy, iterations in imputation_iterations.items():
+    predictions = []
+    for i in range(iterations):
+      # Impute missing data
+      imputed_labs = imputation(strategy, latest_labs, outcomes, train_indices)
+      # Train model and store predictions
+      prediction = train(imputed_labs, outcomes, train_indices, model_name, hyperparameters, regularization)
+      predictions.append(prediction)
+    # Average and store predictions
+    last_train_test_labels = [p['Train Test Label'] for p in predictions][-1]
+    outcome_predictions = pd.concat([p[1] for p in predictions], axis = 1)
+    avg_predictions = pd.concat([outcome_predictions.mean(axis = 1).rename('Mean'), last_train_test_labels], axis = 1)
+    # Write to CSV
+    avg_predictions.to_csv(EXPERIMENT_RESULTS_PATH + 'experiment_results_' + strategy + '.csv')
 
 
 ### Imputation method
-def imputation(imputation_strategy, latest_labs, train_indices, max_iterations = 10):
+def imputation(imputation_strategy, latest_labs, outcomes, train_indices, max_iterations = 10):
   # Single median imputation
   if imputation_strategy == 'Median':
     return latest_labs.fillna(latest_labs.loc[train_indices].median())
@@ -60,8 +63,10 @@ def imputation(imputation_strategy, latest_labs, train_indices, max_iterations =
   # Multiple Imputation using Chained Equation (MICE), Group MICE, and Group MICE Missing
   if 'MICE' in imputation_strategy:
     if 'Missing' in imputation_strategy:
+      counts = latest_labs.isna().add_suffix('_count') # Count columns for Group MICE Missing
       latest_labs = pd.concat([latest_labs, counts], axis = 1)
     if 'Group' in imputation_strategy: 
+      minority_population = pd.concat([outcomes['ETHNICITY'] == 'Black', outcomes['GENDER'] == 'Female', outcomes['INSURANCE'] == 'Public'], axis = 1) # Flag patients in minority population for Group MICE and Group MICE Missing (TRUE = patient is in minority group)
       latest_labs = pd.concat([latest_labs, minority_population], axis = 1)
       
     missing_lab_values = latest_labs.isna()
@@ -92,12 +97,12 @@ def imputation(imputation_strategy, latest_labs, train_indices, max_iterations =
         mice_imputed_labs[lab][missing_lab_values[lab]] = linear_reg_model.predict(x) + epsilon
     
     if 'Group' in imputation_strategy:
-      mice_imputed_labs = mice_imputed_labs.rename(columns={'ETHNICITY': ('VALUENUM', 'ETHNICITY'), 'GENDER': ('VALUENUM', 'GENDER'), 'INSURANCE': ('VALUENUM', 'INSURANCE')})  
+      mice_imputed_labs = mice_imputed_labs.rename(columns={'ETHNICITY': ('VALUENUM', 'ETHNICITY'), 'GENDER': ('VALUENUM', 'GENDER'), 'INSURANCE': ('VALUENUM', 'INSURANCE')})
     return mice_imputed_labs
 
 
 ### Model training method
-def train(imputed_labs, outcomes, all_train_indices, model_name, hyperparameters, regularization = True, normalization = True):
+def train(imputed_labs, outcomes, all_train_indices, model_name, hyperparameters, regularization):
   # Split training data into train, validation, and test
   train_index, test_index = train_test_split(all_train_indices, train_size = 0.9, random_state = 0)
   train_index, validation_index = train_test_split(train_index, train_size = 0.9, random_state = 0)
@@ -114,11 +119,10 @@ def train(imputed_labs, outcomes, all_train_indices, model_name, hyperparameters
   validation_outcomes = outcomes['Outcome'].loc[validation_index]
   
   # Normalization
-  if normalization:
-    normalizer = StandardScaler().fit(train_imputed_labs)
-    train_imputed_labs = pd.DataFrame(normalizer.transform(train_imputed_labs), index = train_imputed_labs.index)
-    validation_imputed_labs = pd.DataFrame(normalizer.transform(validation_imputed_labs), index = validation_imputed_labs.index)
-    imputed_labs = pd.DataFrame(normalizer.transform(imputed_labs), index = imputed_labs.index)
+  normalizer = StandardScaler().fit(train_imputed_labs)
+  train_imputed_labs = pd.DataFrame(normalizer.transform(train_imputed_labs), index = train_imputed_labs.index)
+  validation_imputed_labs = pd.DataFrame(normalizer.transform(validation_imputed_labs), index = validation_imputed_labs.index)
+  imputed_labs = pd.DataFrame(normalizer.transform(imputed_labs), index = imputed_labs.index)
   
   # Regularization for Logisitic Regression
   if (model_name == 'logistic_regression') & (not regularization):
@@ -149,15 +153,13 @@ def train(imputed_labs, outcomes, all_train_indices, model_name, hyperparameters
   return result
 
 
-### Select model parameters
-# Set parameters
+### Select model parameters and run experiment
 imputation_iterations = {
     'Median': 100,
     'MICE': 10,
     'Group MICE': 10,
     'Group MICE Missing': 10
 }
-
 hyperparameters = {
     'logistic_regression': {
         'penalty': ['l2'],
@@ -173,34 +175,9 @@ hyperparameters = {
         'max_iter': [1000]
     }
 }
-# Ablation: Remove regularization
-regularization = True
-# regularization = False
-
-# Experiment: Replace logistic regression with MLP Classifier
-model_name = 'logistic_regression'
-# model_name = 'mlp_classifier'
-
-
-### Impute data, train model, and store predictions
-for strategy, iterations in imputation_iterations.items():
-  print('Imputation strategy:', strategy)
-  print('Iterations: ' + str(iterations))
-
-  predictions = []
-  for i in range(iterations):
-    # Impute missing data
-    imputed_labs = imputation(strategy, latest_labs, train_indices)
-    # Train model and store predictions
-    prediction = train(imputed_labs, outcomes, train_indices, model_name, hyperparameters, regularization)
-    predictions.append(prediction)
-  
-  # Average multiple imputations models
-  last_train_test_labels = [p['Train Test Label'] for p in predictions][-1]
-  predictions_death_values = pd.concat([p[1] for p in predictions], axis = 1)
-  result = pd.concat([predictions_death_values.mean(axis = 1).rename('Mean'), predictions_death_values.std(axis = 1).rename('Std'), last_train_test_labels], axis = 1)
-  # Write to CSV
-  result.to_csv(EXPERIMENT_RESULTS_PATH + 'experiment_results_' + strategy + '.csv')
+# Ablation: To remove regularization, call experiment method with regularization = False
+# Experiment: To test MLP Classifier, call experiment method with model_name = 'mlp_classifier'
+experiment(imputation_iterations, hyperparameters, regularization = True, model_name = 'logistic_regression')
 
 
 ### Report time and memory usage
